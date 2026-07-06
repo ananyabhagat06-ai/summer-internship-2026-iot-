292ba2a4946c0828a6e2c925662f0821c73e1c6e
ananyabhagat06-ai <ananyabhagat06@gmail.com>
2026-07-07 01:06:12 +0530

    Create config.h

---
+++ Assignment /Q35_Weather_Fetcher/config.h
@@ -0,0 +1,12 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+const char* ssid = "YOUR_WIFI";
+const char* password = "YOUR_PASSWORD";
+
+const char* apiKey = "99b9ee8cae1a25dbcef09f3ac6aa36d9";
+
+String city = "Jammu";
+String country = "IN";
+
+#endif
